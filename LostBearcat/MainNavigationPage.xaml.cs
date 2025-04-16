ab6775b32c7b94b67e9cbb1fb5c40c6283f43c45
Fishy0 <jaecar2020@gmail.com>
2025-04-16 05:49:12 -0400

    Changed to remove ItemInformation page and add a log in text entry that verifies an M number (length of 8, starts with capital M)

--- LostBearcat/MainNavigationPage.xaml.cs
+++ LostBearcat/MainNavigationPage.xaml.cs
@@ -6,6 +6,13 @@ namespace LostBearcat
 {
     public partial class MainNavigationPage : ContentPage
     {
+
+
+
+        public bool validLogIn;
+
+
+
         public MainNavigationPage()
         {
             InitializeComponent(); // Make sure this matches your XAML
@@ -13,23 +20,21 @@ namespace LostBearcat
 
         private async void OnAddNewItemClicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new AddItemPage());
+            if (validLogIn)
+            {
+                await Navigation.PushAsync(new AddItemPage());
+            }
         }
 
         private async void OnViewLostItemsClicked(object sender, EventArgs e)
         {
-            var lostItemList = new ViewLostPage();
-            await Navigation.PushAsync(lostItemList);
+            if (validLogIn)
+            {
+                var lostItemList = new ViewLostPage();
+                await Navigation.PushAsync(lostItemList);
+            }
         }
 
-        private async void OnItemInformationClicked(object sender, EventArgs e)
-        {
-            // TODO: UPDATE INFO HERE TO NAVIGATE TO ITEM DETAILS PAGE, possibly more
-            // 
-            // await Navigation.PushAsync(__________);
-            // 
-            //
-        }
 
         private async void OnFilterItemsClicked(object sender, EventArgs e)
         {
@@ -39,5 +44,30 @@ namespace LostBearcat
             // 
             //
         }
+
+
+        // LOGIN Methods
+
+        private void logInAttempt(object sender, TextChangedEventArgs e)
+        {
+            // Get the text entered in the Entry field
+            var input = logIn.Text;
+
+            // Check if the input is 8 characters long and starts with 'M'
+            if (input.Length == 8 && input.StartsWith("M"))
+            {
+                // Input is valid
+                validLogIn = true;
+            }
+            else
+            {
+                // Input is invalid
+                validLogIn = false;
+            }
+        }
+
+
     }
+
+
 }
